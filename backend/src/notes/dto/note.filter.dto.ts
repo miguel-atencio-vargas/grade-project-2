716afd81b0ac0ca85ef78cac51761f8376ca585b
Miguel Atencio <atencio947@gmail.com>
2025-12-04 21:19:04 -0400

    fix: add boolean transformation for note filter query parameters

--- backend/src/notes/dto/note.filter.dto.ts
+++ backend/src/notes/dto/note.filter.dto.ts
@@ -1,13 +1,16 @@
 import { IsOptional } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class NoteFilterDTO {
   @IsOptional()
   uuid?: string;
 
   @IsOptional()
+  @Transform(({ value }) => value === 'true')
   isDeleted?: boolean = false;
 
   @IsOptional()
+  @Transform(({ value }) => value === 'true')
   isArchived?: boolean;
 }
 
