46feeba80b2c8c85ea1bccea068fe26773c9aea9
Miguel Atencio <atencio947@gmail.com>
2025-12-05 01:04:48 -0400

    fix: update home link to point to index.html explicitly

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -10,12 +10,12 @@ export const metadata: Metadata = {
   description: "Notepad app created with Next JS by Miguel Atencio",
 };
 
-export default function RootLayout({children}: Readonly<{ children: React.ReactNode; }>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="en">
       <body className={`${inter.className} h-screen flex items-center justify-center flex-col`} >
         <header className="text-4xl">
-          <GoToPage href={'.'} text={'Notepad App'} />
+          <GoToPage href={'./index.html'} text={'Notepad App'} />
         </header>
         {children}
       </body>
